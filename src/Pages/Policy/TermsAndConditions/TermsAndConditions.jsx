278594b88164545a9f0e63117ac7775ba0f7da46
AbhiMane07012000 <abhijeet.nitin.mane@gmail.com>
2025-01-10 17:58:10 +0530

    Terms and conditions had Completed.

--- src/Pages/Policy/TermsAndConditions/TermsAndConditions.jsx
+++ src/Pages/Policy/TermsAndConditions/TermsAndConditions.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TermsAndConditions = () => {
   return (
-    <div className="h-[60vh] md:h-full lg:h-[235vh] px-4 pt-20 lg:pt-20 lg:px-40 ">
+    <div className="h-[60vh] md:h-full lg:h-[234vh] px-4 pt-20 lg:pt-20 lg:px-40 ">
       <h2 className="text-2xl font-medium">Terms and Conditions</h2>
       <strong className="py-2">
         This website is operated by Arcon Associates.
